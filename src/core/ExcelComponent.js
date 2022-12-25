fd5654000f5e7839247b7f2cc7b83f5fee98ef34
abdulhay777 <75833188+abdulhay777@users.noreply.github.com>
2022-12-25 16:32:11 +0500

    finish logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,14 +4,26 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name
+    this.emitter = options.emitter
+    this.unsub = []
+    this.prepare()
   }
   toHTML() {
     return ''
+  }
+  prepare() {}
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsub.push(unsub)
   }
   init() {
     this.initDOMListeners()
   }
   destroy() {
+    this.unsub.forEach(e => e())
     this.removeDOMListeners()
   }
 }
